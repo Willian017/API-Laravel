2309493cf2e53f75cc2639c74a7496658bfb4223
willian017 <willianyuji100@gmail.com>
2025-08-29 17:32:06 -0300

    Add custom exception handler for application
    - Created a new exception handler class in the App\Exceptions namespace.
    - Implemented unauthenticated method to handle authentication exceptions.
    - Added JSON response for unauthenticated API requests.
    - Configured properties to specify which exceptions should not be reported and which inputs should not be flashed to the session.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,27 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->shouldRenderJsonWhen(function ($request, Throwable $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+            return $request->expectsJson();
+        });
+
+        $exceptions->renderable(function (AuthenticationException $e, $request) {
+            return response()->json(['message' => 'Usuário não autenticado'], 401);
+        });
+
+        $exceptions->renderable(function (ValidationException $e, $request) {
+            return response()->json([
+                'message' => 'Erro de validação',
+                'errors'  => $e->errors(),
+            ], 422);
+        });
+
+        $exceptions->renderable(function (ModelNotFoundException $e, $request) {
+            return response()->json([
+                'message' => 'Registro não encontrado'
+            ], 404);
+        });
     })->create();
